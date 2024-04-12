91dcc2343fdea7e63099e15ef66c4c967c4f3627
May <MayflowerC10@gmail.com>
2024-04-12 12:57:42 -0400

    Add file header size test

--- lib/frieren/src/tests.rs
+++ lib/frieren/src/tests.rs
@@ -10,16 +10,18 @@ fn trivial() {
 }
 
 const HELLO_WORLD_ELF: &[u8] = include_bytes!("../test-files/test-elf.o");
+const FILE_HEADER_SIZE: usize = 64;
 
 #[test]
-fn check_header() {
+fn file_header_size() {
+    // We are always using 64-bit, so the size of the file header is known
+    assert_eq!(mem::size_of::<FileHeader>(), FILE_HEADER_SIZE);
+}
+
+#[test]
+fn header_loading() {
     println!("First bytes: {:x?}", &HELLO_WORLD_ELF[0..4]);
     let ptr = HELLO_WORLD_ELF as *const [u8] as *const FileHeader;
-    println!("Magic Bytes: {:x?}", unsafe { &*ptr }.magic_bytes);
-    println!("Size: {:?}", {
-        let size = unsafe { &*ptr }.size;
-        size
-    });
     // Some of this should be extracted to a larger ELF struct at some point, but this works for now
     let file_header: Result<&FileHeader, ElfError> = unsafe { 
         FileHeader::try_from_raw(ptr)
@@ -27,3 +29,4 @@ fn check_header() {
     println!("File Header Status: {:?}", file_header.as_ref().err());
     assert!(file_header.is_ok());
 }
+
